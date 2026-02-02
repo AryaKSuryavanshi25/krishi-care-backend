ad8a89cdf72908c0eb869597242a601d1e7b3401
Arya Kalpak Suryavanshi <aryasuryavanshi25@gmail.com>
2026-02-02 23:08:45 +0530

    Initial commit - Krishi Care Backend

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+
+const connectDB = require("./config/db");
+require("./config/passport");
+
+const authRoutes = require("./routes/auth.routes");
+
+const app = express();
+
+// Connect MongoDB
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+
+// Routes
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Krishi Care Backend Running 🚜");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
